5347de9c6787545f9dcca5bf40c6d5fe6cdef6d3
FPGAGROUP2 <55390229+FPGAGROUP2@users.noreply.github.com>
2019-11-08 20:28:04 +0800

    Add files via upload

---
+++ Lab04/Program 1/src/driver/sorting.c
@@ -0,0 +1,15 @@
+
+
+/***************************** Include Files *******************************/
+#include "sorting.h"
+
+/************************** Function Definitions ***************************/
+u32 sorting(UINTPTR BaseAddr, u32 data){
+	SORTING_mWriteReg(BaseAddr, 4, 0);
+	SORTING_mWriteReg(BaseAddr, 0, data);
+	SORTING_mWriteReg(BaseAddr, 4, 1);
+
+	u32 sorting_result = SORTING_mReadReg(BaseAddr, 4);
+
+	return sorting_result;
+}
