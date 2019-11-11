54554ecf14e34f6adcbfd38d13c85b308eb30dee
FPGAGROUP2 <55390229+FPGAGROUP2@users.noreply.github.com>
2019-11-11 10:28:43 +0800

    Add files via upload

--- Lab04/Program 2/test2/src/main.c
+++ Lab04/Program 2/test2/src/main.c
@@ -1,62 +1,49 @@
-/*
- * main.c
- *
- *  Created on: 2019¦~11¤ë8¤é
- *      Author: JJHU
- */
-
 #include<stdio.h>
-#include "xil_printf.h"
-#include "xil_io.h"
-#include "xparameters.h"
-#include "test2.h"
+
 int main()
-{
+(
 
 	//u32	A, B, Sum;
-	u32 A, B, Sum;
-	u32 x;
-	u32 C;//operator signal
+	signed int 32 A, B, Sum;
+	u2 C;//operator signal
 	char D;
-
 		printf("Program Start.\n\r");
+		while(getchar() != EOF) 
+		{
 
-			printf("Operator:");
-			scanf("%c", &D);
-			printf(" %c\r\n", D);
 			printf("Input A:");
 			scanf("%d", &A);
 			printf(" %d\r\n", A);
 			printf("Input B:");
 			scanf("%d", &B);
 			printf(" %d\r\n", B);
-
-
+			printf("Operator:");
+			scanf("%c", &E);
+			printf(" %c\r\n", E);
+			
 			if(D =='+')
 			{
 				C = 0;
-				Sum = test2(XPAR_TEST2_0_S00_AXI_BASEADDR, A, B, C);
+				Sum = add(XPAR_PRO2_0_S00_AXI_BASEADDR, A, B);
 				printf("A + B = %d\n\r", Sum);
 			}
 			if(D =='-')
 			{
 				C = 1;
-				Sum = test2(XPAR_TEST2_0_S00_AXI_BASEADDR, A, B, C);
+				Sum = add(XPAR_PRO2_0_S00_AXI_BASEADDR, A, B);
 				printf("A - B = %d\n\r", Sum);
 			}
 			if(D =='*')
 			{
 				C =2;
-				Sum = test2(XPAR_TEST2_0_S00_AXI_BASEADDR, A, B, C);
+				Sum = add(XPAR_PRO2_0_S00_AXI_BASEADDR, A, B);
 				printf("A * B = %d\n\r", Sum);
 			}
+			
 
-			x = TEST2_mReadReg (XPAR_TEST2_0_S00_AXI_BASEADDR, 8);
-			printf("%d\n\r",x);
-
+		}
 		printf("Program End.\n\r");
   		return 0;
 
 
-}
-
+) 
